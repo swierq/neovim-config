3a2b90daa1945f7a624673ad122a17364d1a550d
Przemyslaw Swierczek <swierq@gmail.com>
2024-04-21 01:14:44 +0200

    copilot VeryLazy

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,12 +1,12 @@
 return {
-  {
-    "github/copilot.vim",
-    --    event = "VeryLazy",
-    version = "*",
-    config = function()
-      vim.keymap.set('i', '<F1>', 'copilot#Accept("<CR>")', { expr = true, silent = true })
-      vim.keymap.set('i', '<F2>', 'copilot#Next()', { expr = true, silent = true })
-      vim.keymap.set('i', '<F3>', 'copilot#Previous()', { expr = true, silent = true })
-    end,
-  },
+	{
+		"github/copilot.vim",
+		event = "VeryLazy",
+		version = "*",
+		config = function()
+			vim.keymap.set("i", "<F1>", 'copilot#Accept("<CR>")', { expr = true, silent = true })
+			vim.keymap.set("i", "<F2>", "copilot#Next()", { expr = true, silent = true })
+			vim.keymap.set("i", "<F3>", "copilot#Previous()", { expr = true, silent = true })
+		end,
+	},
 }
