f8a4b8fc8962bb8042499d2f77ed1f03ebab875f
Przemyslaw Swierczek <swierq@gmail.com>
2024-04-21 11:01:11 +0200

    trimm config

--- lua/plugins/trim.lua
+++ lua/plugins/trim.lua
@@ -1,4 +1,17 @@
 return {
-  "cappyzawa/trim.nvim",
-  opts = {}
+	"cappyzawa/trim.nvim",
+	config = function()
+		require("trim").setup({
+			patterns = {
+				[[%s/\(\n\n\)\n\+/\1/]], -- replace multiple blank lines with a single line
+			},
+
+			-- if you want to disable trim on write by default
+			trim_on_write = true,
+			trim_last_line = false,
+
+			-- highlight trailing spaces
+			highlight = true,
+		})
+	end,
 }
