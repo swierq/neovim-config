c90a007f6032b91d4ebc11dd3fda4f19cbee09ff
Przemyslaw Swierczek <swierq@gmail.com>
2024-04-24 22:20:16 +0200

    Pane navigation

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,3 +5,12 @@ vim.cmd("set shiftwidth=2")
 vim.cmd("set autoindent")
 vim.cmd("set number")
 vim.g.mapleader = " "
+vim.keymap.set("n", "=", [[<cmd>vertical resize +5<cr>]]) -- make the window biger verticallydd
+vim.keymap.set("n", "-", [[<cmd>vertical resize -5<cr>]]) -- make the window smaller vertically
+vim.keymap.set("n", "+", [[<cmd>horizontal resize +2<cr>]]) -- make the window bigger horizontally by pressing shift and =
+vim.keymap.set("n", "_", [[<cmd>horizontal resize -2<cr>]]) -- make the window smaller horizontally by pressing shift and -
+
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
