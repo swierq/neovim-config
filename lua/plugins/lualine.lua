cecfcf051aa3164e286dae4b33c6b91dfe5af9cb
Przemyslaw Swierczek <swierq@gmail.com>
2024-05-12 18:58:14 +0200

    lualine relative path

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,26 +5,26 @@ return {
 		require("lualine").setup({
 			options = {
 				theme = "dracula",
-				sections = {
-					lualine_c = {
-						{
-							"filename",
-							file_status = true, -- Displays file status (readonly status, modified status)
-							newfile_status = false, -- Display new file status (new file means no write after created)
-							path = 2, -- 0: Just the filename
-							-- 1: Relative path
-							-- 2: Absolute path
-							-- 3: Absolute path, with tilde as the home directory
-							-- 4: Filename and parent dir, with tilde as the home directory
+			},
+			sections = {
+				lualine_c = {
+					{
+						"filename",
+						file_status = true, -- Displays file status (readonly status, modified status)
+						newfile_status = false, -- Display new file status (new file means no write after created)
+						path = 1, -- 0: Just the filename
+						-- 1: Relative path
+						-- 2: Absolute path
+						-- 3: Absolute path, with tilde as the home directory
+						-- 4: Filename and parent dir, with tilde as the home directory
 
-							shorting_target = 40, -- Shortens path to leave 40 spaces in the window
-							-- for other components. (terrible name, any suggestions?)
-							symbols = {
-								modified = "[+]", -- Text to show when the file is modified.
-								readonly = "[-]", -- Text to show when the file is non-modifiable or readonly.
-								unnamed = "[No Name]", -- Text to show for unnamed buffers.
-								newfile = "[New]", -- Text to show for newly created file before first write
-							},
+						shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+						-- for other components. (terrible name, any suggestions?)
+						symbols = {
+							modified = "[+]", -- Text to show when the file is modified.
+							readonly = "[-]", -- Text to show when the file is non-modifiable or readonly.
+							unnamed = "[No Name]", -- Text to show for unnamed buffers.
+							newfile = "[New]", -- Text to show for newly created file before first write
 						},
 					},
 				},
