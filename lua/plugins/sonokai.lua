ef56d5d3fc07cbca03a42b8d0b0dd59ec9267621
Przemyslaw Swierczek <swierq@gmail.com>
2024-05-12 10:04:12 +0200

    extracting theme file

--- lua/plugins/sonokai.lua
+++
@@ -1,8 +0,0 @@
-return {
-	--   "sainnhe/sonokai",
-	--   name = "sonokai",
-	--   priority = 1000,
-	--   config = function()
-	--     vim.cmd.colorscheme("sonokai")
-	--   end,
-}
---
+++ lua/plugins/theme.lua
@@ -0,0 +1,34 @@
+return {
+	-- {
+	--   "catppuccin/nvim",
+	--   name = "catppuccin",
+	--   priority = 1000,
+	--   config = function()
+	--     vim.cmd.colorscheme("catppuccin-mocha")
+	--   end,
+	-- },
+	-- {
+	--   "npxbr/gruvbox.nvim",
+	--   name = "gruvbox",
+	--   priority = 1000,
+	--   config = function()
+	--     vim.cmd.colorscheme("gruvbox")
+	--   end,
+	-- },
+	-- {
+	-- 	"folke/tokyonight.nvim",
+	-- 	name = "tokyonight",
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		vim.cmd.colorscheme("tokyonight")
+	-- 	end,
+	-- },
+	{
+		"Mofiqul/dracula.nvim",
+		name = "dracula",
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("dracula")
+		end,
+	},
+}
