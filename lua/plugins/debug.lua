df92b0c5d4d7a76c0b48ae9a8f619e9127bdd347
Przemyslaw Swierczek <swierq@gmail.com>
2024-05-16 20:28:51 +0200

    Added nvim oil

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -1,29 +1,29 @@
 return {
-	"mfussenegger/nvim-dap",
-	dependencies = {
-		"rcarriga/nvim-dap-ui",
-		"leoluz/nvim-dap-go",
-		"nvim-neotest/nvim-nio",
-	},
-	config = function()
-		local dap = require("dap")
-		local dapui = require("dapui")
-		require("dapui").setup()
-		require("dap-go").setup()
-		dap.listeners.before.attach.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.launch.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.event_terminated.dapui_config = function()
-			dapui.close()
-		end
-		dap.listeners.before.event_exited.dapui_config = function()
-			dapui.close()
-		end
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "rcarriga/nvim-dap-ui",
+    "leoluz/nvim-dap-go",
+    "nvim-neotest/nvim-nio",
+  },
+  config = function()
+    local dap = require("dap")
+    local dapui = require("dapui")
+    require("dapui").setup()
+    require("dap-go").setup()
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
 
-		vim.keymap.set("n", "<leader>dt", dap.toggle_breakpoint, {})
-		vim.keymap.set("n", "<leader>dc", dap.continue, {})
-	end,
+    vim.keymap.set("n", "<leader>dt", dap.toggle_breakpoint, {})
+    vim.keymap.set("n", "<leader>dc", dap.continue, {})
+  end,
 }
