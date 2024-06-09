53c31a6d57a8b0a13ddeb7a4b4b4641fc00218f9
Przemyslaw Swierczek <swierq@gmail.com>
2024-06-09 17:00:23 +0200

    Debug f5-f9

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -1,29 +1,28 @@
 return {
-  "mfussenegger/nvim-dap",
-  dependencies = {
-    "rcarriga/nvim-dap-ui",
-    "leoluz/nvim-dap-go",
-    "nvim-neotest/nvim-nio",
-  },
-  config = function()
-    local dap = require("dap")
-    local dapui = require("dapui")
-    require("dapui").setup()
-    require("dap-go").setup()
-    dap.listeners.before.attach.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.launch.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.event_terminated.dapui_config = function()
-      dapui.close()
-    end
-    dap.listeners.before.event_exited.dapui_config = function()
-      dapui.close()
-    end
-
-    vim.keymap.set("n", "<leader>dt", dap.toggle_breakpoint, {})
-    vim.keymap.set("n", "<leader>dc", dap.continue, {})
-  end,
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+		"leoluz/nvim-dap-go",
+		"nvim-neotest/nvim-nio",
+	},
+	config = function()
+		local dap = require("dap")
+		local dapui = require("dapui")
+		require("dapui").setup()
+		require("dap-go").setup()
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+		vim.keymap.set("n", "<f9>", dap.toggle_breakpoint, {})
+		vim.keymap.set("n", "<f5>", dap.continue, {})
+	end,
 }
