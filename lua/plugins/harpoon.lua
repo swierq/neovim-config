40c125c0535792d9f6e4130bceafe47bc3c49acc
Przemyslaw Swierczek <swierq@gmail.com>
2024-10-15 16:36:51 +0200

    whichkey

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -9,9 +9,9 @@ return {
 		})
 		local mark = require("harpoon.mark")
 		local ui = require("harpoon.ui")
-		vim.keymap.set("n", "<leader>xa", mark.add_file, {})
-		vim.keymap.set("n", "<leader>xm", ui.toggle_quick_menu, {})
-		vim.keymap.set("n", "<leader>xp", ui.nav_next, {})
-		vim.keymap.set("n", "<leader>xn", ui.nav_prev, {})
+		vim.keymap.set("n", "<leader>xa", mark.add_file, { desc = "Harpoon add file" })
+		vim.keymap.set("n", "<leader>xm", ui.toggle_quick_menu, { desc = "Harpoon menu" })
+		vim.keymap.set("n", "<leader>xp", ui.nav_next, { desc = "Harpoon next" })
+		vim.keymap.set("n", "<leader>xn", ui.nav_prev, { desc = "Harpoon prev" })
 	end,
 }
