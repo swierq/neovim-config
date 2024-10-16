24ec2fb0344b9b37f1aa2cce0172aac391f471fb
Przemyslaw Swierczek <swierq@gmail.com>
2024-10-16 22:12:46 +0200

    treesitter updates

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,37 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local configs = require("nvim-treesitter.configs")
-    configs.setup({
-      -- ensure_installed = { "go", "lua", "vim", "vimdoc", "templ", "html", "css", "terraform"},
-      auto_install = true,
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
+		configs.setup({
+			ensure_installed = {
+				"go",
+				"lua",
+				"vim",
+				"vimdoc",
+				"templ",
+				"html",
+				"css",
+				"terraform",
+				"javascript",
+				"typescript",
+				"tsx",
+				"bash",
+				"css",
+				"templ",
+				"dockerfile",
+				"make",
+				"python",
+				"tmux",
+				"yaml",
+				"json",
+				"xml",
+				"toml",
+			},
+			auto_install = true,
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
