2a9205ca358b5e4ba6dc3e5ca46837b13b21c1b2
Przemyslaw Swierczek <swierq@gmail.com>
2024-12-30 18:50:26 +0100

    updates

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,48 +1,48 @@
 local function yaml_ft(path, bufnr)
-	-- get content of buffer as string
-	-- local content = vim.filetype.getlines(bufnr)
-	local content = vim.api.nvim_buf_get_lines(0, 0, vim.api.nvim_buf_line_count(0), false)
+  -- get content of buffer as string
+  -- local content = vim.filetype.getlines(bufnr)
+  local content = vim.api.nvim_buf_get_lines(0, 0, vim.api.nvim_buf_line_count(0), false)
 
-	if type(content) == "table" then
-		content = table.concat(content, "\n")
-	end
+  if type(content) == "table" then
+    content = table.concat(content, "\n")
+  end
 
-	-- check if file is in roles, tasks, or handlers folder
-	local path_regex = vim.regex("(tasks\\|roles\\|handlers)/")
-	if path_regex and path_regex:match_str(path) then
-		return "yaml.ansible"
-	end
-	-- check for known ansible playbook text and if found, return yaml.ansible
-	-- local regex = vim.regex("hosts:\\|tasks:")
-	-- if regex and regex:match_str(content) then
-	-- 	return "yaml.ansible"
-	-- end
-	--
-	-- -- return yaml if nothing else
-	return "yaml"
+  -- check if file is in roles, tasks, or handlers folder
+  local path_regex = vim.regex("(tasks\\|roles\\|handlers)/")
+  if path_regex and path_regex:match_str(path) then
+    return "yaml.ansible"
+  end
+  -- check for known ansible playbook text and if found, return yaml.ansible
+  -- local regex = vim.regex("hosts:\\|tasks:")
+  -- if regex and regex:match_str(content) then
+  -- 	return "yaml.ansible"
+  -- end
+  --
+  -- -- return yaml if nothing else
+  return "yaml"
 end
 
 vim.filetype.add({
-	extension = {
-		templ = "templ",
-		yml = yaml_ft,
-		yaml = yaml_ft,
-	},
-	filename = {
-		["Jenkinsfile"] = "groovy",
-	},
+  extension = {
+    templ = "templ",
+    yml = yaml_ft,
+    yaml = yaml_ft,
+  },
+  filename = {
+    ["Jenkinsfile"] = "groovy",
+  },
 })
 
 vim.api.nvim_create_user_command(
-	"Aen",
-	"!ansible-vault encrypt %",
-	{ bang = true, desc = "Encrypt file with ansible-vault" }
+  "Aen",
+  "!ansible-vault encrypt %",
+  { bang = true, desc = "Encrypt file with ansible-vault" }
 )
 
 vim.api.nvim_create_user_command(
-	"Ade",
-	"!ansible-vault decrypt %",
-	{ bang = true, desc = "Encrypt file with ansible-vault" }
+  "Ade",
+  "!ansible-vault decrypt %",
+  { bang = true, desc = "Encrypt file with ansible-vault" }
 )
 
 vim.opt.spelllang = "en_us"
