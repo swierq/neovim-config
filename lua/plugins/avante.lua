83669e51481727719903e3c1f6427fdf2e7f3df7
Przemyslaw Swierczek <swierq@gmail.com>
2025-07-17 19:47:38 +0200

    updates and fixes

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -7,12 +7,16 @@ return {
 		-- add any opts here
 		-- for example
 		provider = "copilot",
-		openai = {
-			endpoint = "https://api.openai.com/v1",
-			model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
-			timeout = 30000, -- timeout in milliseconds
-			temperature = 0, -- adjust if needed
-			max_tokens = 4096,
+		providers = {
+			openai = {
+				endpoint = "https://api.openai.com/v1",
+				model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
+				timeout = 30000, -- timeout in milliseconds
+				extra_request_body = {
+					temperature = 0, -- adjust if needed
+				},
+				max_tokens = 4096,
+			},
 		},
 	},
 	-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
