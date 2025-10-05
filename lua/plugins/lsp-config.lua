2259b5ff2fb25c14dbc1f6c8da990d21e5a51412
Przemyslaw Swierczek <swierq@gmail.com>
2025-10-05 21:16:58 +0200

    updaes

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -30,7 +30,6 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
-			-- local lspconfig = require("lspconfig")
 			local opts = {}
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			vim.lsp.config("lua_ls", {
